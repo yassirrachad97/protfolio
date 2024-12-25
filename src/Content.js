0c5c8d1258544f5fc21d351b02964de2eb7ca49e
Yassir Rachad <yassirrachad1997@gmail.com>
2024-12-25 11:22:42 +0100

    new changes

--- src/Content.js
+++ src/Content.js
@@ -12,11 +12,11 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "../src/assets/images/Projects/img1.png";
+import project1 from "./assets/images/Projects/img1.png";
 
-import project2 from "../src/assets/images/projects/img2.png";
-import project3 from "../src/assets/images/projects/img3.png";
-import person_project from "../src/assets/images/projects/person.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
